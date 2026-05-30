bdf9dcf0efb2c3d3f4161c24543dc040735fb608
Lakshay Manchanda <lakshaymanchanda06@gmail.com>
2026-05-30 13:32:16 -0700

    done till loss

--- neuron.py
+++ neuron.py
@@ -8,8 +8,6 @@ np.random.seed(0)
 
 # X = np.array([[1, 2, 3, 2.5], [2, 5, -1, 2], [-1.5, 2.7, 3.3, -0.8]])
 
-X, y = spiral_data(100, 3)  # 100 data points that 2 features each
-
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -25,10 +23,58 @@ class Activation_ReLU:
         self.output = np.maximum(0, inputs)
 
 
-layer1 = Layer_Dense(2, 5)
-layer1.forward(X)
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(
+            inputs - np.max(inputs, axis=1, keepdims=True)
+        )  # this ensures that we keeping the dimensions the same, meaning that the max is not calculated overall the batch outputs but per outputs. And then ofcourse the axis is related to which axis to calcualte the max in(here we are taking it as row)
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
 
+
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        # need to check if the categories passed are scalar or a hot one encoded vector
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape == 2):
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+
+        negative_log_likelihood = -np.log(correct_confidences)
+        return negative_log_likelihood
+
+
+X, y = spiral_data(
+    samples=100, classes=3
+)  # 100 data points that has 2 features each (x and y coordinates)
+
+
+dense1 = Layer_Dense(2, 3)
 activation1 = Activation_ReLU()
 
-activation1.forward(layer1.output)
-print(activation1.output)
+dense2 = Layer_Dense(
+    3, 3
+)  # Again, input needs to be 3 since the previous layer output was 3.
+
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+
+loss_function = Loss_CategoricalCrossentropy()
+loss = loss_function.calculate(activation2.output, y)
+
+print("Loss:", loss)
